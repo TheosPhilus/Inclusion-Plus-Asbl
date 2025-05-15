f17ac4d358f677159f3326e4794755e79b36461e
Théophile ZOSSOU <topophilus@gmail.com>
2025-05-15 11:02:27 +0200

    Version2.9

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("footer.html") // Charge le fichier footer
+        .then(response => response.text()) // Convertit en texte
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data; // Injecte le contenu dans la div cible
+        })
+        .catch(error => console.error("Erreur lors du chargement du footer :", error));
+});
